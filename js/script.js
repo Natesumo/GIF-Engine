97242e4ab40ad39d4f2689950e3dd393689641d7
Natesumo <natesumo@yahoo.com>
2020-01-17 13:59:59 -0500

    Initial Commit
    Working GIPHY API. Worked with jQuery, AJAX, and the giphy developers site to get a working search bar.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function getData(){
+
+    $('.inner').empty();
+
+    var input = $("#searchtext").val();
+
+
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"+&api_key=oTtBtF7UxZQYt1D09suUm42WgB6PxM7w&limit=30");
+
+    xhr.done(function(response) {
+        
+        console.log("success got data", response); 
+
+        var gifs = response.data;
+
+        for (i in gifs)
+        {
+            $('.inner').append("<img src='"+gifs[i].images.original.url+"' style= 'height:350px; width:350px; ' >")
+        }
+        });
+    }
